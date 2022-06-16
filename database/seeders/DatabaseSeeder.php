407010a401934ef300db35dec1c495410ef7f550
Saad Farooq <saad96farooq@gmail.com>
2022-06-16 12:18:00 +0500

    Admin login from added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Admin;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+
+        Admin::create([
+            'name' => 'khizar wahab',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+        ]);
     }
 }
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,98 +1,75 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <title>Login Form</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            background-color: #6abadeba;
-            font-family: 'Arial';
-        }
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Log in (v2)</title>
 
-        .login {
-            width: 382px;
-            overflow: hidden;
-            margin: auto;
-            margin: 20 0 0 450px;
-            padding: 80px;
-            background: #23463f;
-            border-radius: 15px;
-
-        }
-
-        h2 {
-            text-align: center;
-            color: #277582;
-            padding: 20px;
-        }
-
-        label {
-            color: #08ffd1;
-            font-size: 17px;
-        }
-
-        #Uname {
-            width: 300px;
-            height: 30px;
-            border: none;
-            border-radius: 3px;
-            padding-left: 8px;
-        }
-
-        #Pass {
-            width: 300px;
-            height: 30px;
-            border: none;
-            border-radius: 3px;
-            padding-left: 8px;
-
-        }
-
-        #log {
-            width: 300px;
-            height: 30px;
-            border: none;
-            border-radius: 17px;
-            padding-left: 7px;
-            color: blue;
-
-
-        }
-
-        span {
-            color: white;
-            font-size: 17px;
-        }
-
-        a {
-            float: right;
-            background-color: grey;
-        }
-    </style>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('admin_assets/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ asset('admin_assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('admin_assets/dist/css/adminlte.min.css') }}">
 </head>
-
-<body>
-    <h2>Login Page</h2><br>
-    <div class="login">
-        <form id="login" method="post" action="{{ route('admin-auth') }}">
-          @csrf
-            <label><b>User Name
-                </b>
-            </label>
-            <input type="email" name="email" id="email" placeholder="email">
-            <br><br>
-            <label><b>Password
-                </b>
-            </label>
-            <input type="password" name="password" id="password" placeholder="password">
-            <br><br>
-            <span style="color:red">{{ session('error') }}</span>
-            <input type="submit" name="log" id="log" value="Log In Here">
-        </form>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
     </div>
-</body>
+    <div class="card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
 
+      <form id="login" method="post" action="{{ route('admin-auth') }}">
+        @csrf
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" name="email" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <span style="color:red">{{ session('error') }}</span>
+        <div class="row">
+          <div class="col-8">
+            
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <p class="mb-1">
+        <a href="forgot-password.html">I forgot my password</a>
+      </p>
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="{{ asset('admin_assets/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('admin_assets/dist/js/adminlte.min.js') }}"></script>
+</body>
 </html>
