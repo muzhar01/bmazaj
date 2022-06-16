a4e9f44f04a1136578eb9a79199b029721d3c5d3
admk80 <hariskh5512@gmail.com>
2022-06-16 12:55:38 +0500

    admin_dashboard_layout

--- app/Http/Controllers/Admin/AdminDashboard.php
+++ app/Http/Controllers/Admin/AdminDashboard.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class AdminDashboard extends Controller
 {
     public function index(){
-        return view('admin/dashborad');
+        return view('admin/dashboard');
     }
 }
